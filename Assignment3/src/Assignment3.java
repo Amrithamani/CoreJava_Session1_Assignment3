0550554c508c822d5a0d67844b4c4d40063c1052
Amrithamani <amrimani91@gmail.com>
2017-05-25 15:50:08 -0400

    CoreJava_Session1_Assignment3
    CoreJava_Session1_Assignment3 Code

---
+++ Assignment3/src/Assignment3.java
@@ -0,0 +1,24 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 10;
+		int b = 30;
+		
+		System.out.println("Before swapping: a = " + a);
+		System.out.println("Before swapping: b = " + b);
+		
+		a = a + b;
+		
+		b = a - b;
+		
+		a = a - b;
+		
+		System.out.println("After swapping: a = " + a);
+		System.out.println("After swapping: b = " + b);
+
+	}
+
+}
